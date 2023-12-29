5b61ce766b99caf5c514a2130b1337e869530e02
georgek1205 <georgek1205@naver.com>
2023-12-29 17:06:36 +0900

    Added dowork# labels2

--- src/main/java/com/example/subway2/controller/SubwayStationInfoController.java
+++ src/main/java/com/example/subway2/controller/SubwayStationInfoController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class SubwayStationInfoController {
-    //start_work1()
+    //start_work()
     private final SubwayStationInfoRepository subwayStationInfoRepository;
 
     @RequestMapping("/subwayinfo")
